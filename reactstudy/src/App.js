ee9241b566c819d4e1a200817cb6de2d46aebc33
김희원 <julian3306@naver.com>
2023-05-15 19:13:27 +0900

    feat schedule

---
+++ reactstudy/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItem';
+function App() {
+  return (
+    <div className='App'>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
