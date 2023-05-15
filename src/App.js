088f551c43683c06264f876c1c2c92dd8b4e47bc
eeee_day_ <joonrose0221@gmail.com>
2023-05-15 18:39:26 +0900

    feat login

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Mylogin from './login'
+
+function App() {
+  return (
+    <div className="App">
+      <Mylogin />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/login.js
@@ -0,0 +1,12 @@
+const Mylogin = () =>{
+    return (
+        <div className="mainbox">
+            <div className="photo_box">사진</div>
+            <div className="login_box">로그인창</div>
+        </div>
+
+    );
+}
+
+
+export default Mylogin;
