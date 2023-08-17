979bebaaa88bcc03014288fb0c2ea11d747cd41d
eeee_day_ <joonrose0221@gmail.com>
2023-08-18 02:09:19 +0900

    feat mainpage

--- src/gridlayout.js
+++ src/gridlayout.js
@@ -1,17 +1,18 @@
+import React, { useState } from "react";
 import GridLayout from "react-grid-layout";
-
 import styled from "styled-components";
 
 const layout = [
-  { i: "blue-eyes-dragon", x: 0, y: 0, w: 1, h: 1 },
-  { i: "dark-magician", x: 1, y: 0, w: 1, h: 1 },
-  { i: "kuriboh", x: 2, y: 0, w: 1, h: 1 },
-  { i: "spell-caster", x: 3, y: 0, w: 1, h: 1 },
-  { i: "summoned-skull", x: 4, y: 0, w: 1, h: 1 }
+  { i: "Japan", x: 0, y: 0, w: 1, h: 1 },
+  { i: "Vietnam", x: 1, y: 0, w: 1, h: 1 },
+  { i: "Jeju", x: 2, y: 0, w: 1, h: 1 },
+  { i: "+", x: 3, y: 0, w: 1, h: 1 },
 ];
 
 const GridItemWrapper = styled.div`
   background: #f5f5f5;
+  cursor: pointer; /* 커서 모양을 포인터로 변경하여 클릭 가능한 요소임을 표시 */
+  border: 2px solid ${(props) => (props.selected ? "#0099a4" : "transparent")};
 `;
 
 const GridItemContent = styled.div`
@@ -22,28 +23,39 @@ const Root = styled.div`
   padding: 16px;
 `;
 
-export const Grid = () => {
+const GridComponent = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (item) => {
+    if (selectedItem === item) {
+      // 이미 선택된 상태에서 더블클릭하면 해당 URL로 이동
+      window.location.href = `/${item}`;
+    } else {
+      setSelectedItem(item);
+    }
+  };
+
   return (
     <Root>
-      <GridLayout layout={layout} cols={5} rowHeight={300} width={1000}>
-        <GridItemWrapper key="blue-eyes-dragon">
-          <GridItemContent>Blue Eyes Dragon</GridItemContent>
-        </GridItemWrapper>
-        <GridItemWrapper key="dark-magician">
-          <GridItemContent>Dark Magician</GridItemContent>
-        </GridItemWrapper>
-        <GridItemWrapper key="kuriboh">
-          <GridItemContent>Kuriboh</GridItemContent>
-        </GridItemWrapper>
-        <GridItemWrapper key="spell-caster">
-          <GridItemContent>Spell Caster</GridItemContent>
-        </GridItemWrapper>
-        <GridItemWrapper key="summoned-skull">
-          <GridItemContent>Summoned Skull</GridItemContent>
-        </GridItemWrapper>
+      <GridLayout layout={layout} cols={4} rowHeight={100} width={1000}>
+        {layout.map((item) => (
+          <GridItemWrapper
+            key={item.i}
+            onClick={() => handleItemClick(item.i)}
+            onDoubleClick={() => handleItemClick(item.i)} // 더블클릭 시에 해당 url로 넘어감
+            style={{
+              border:
+                selectedItem === item.i
+                  ? "2px solid #0099a4"
+                  : "2px solid gray",
+            }} //테두리 효과 주지말까
+          >
+            <GridItemContent>{item.i}</GridItemContent>
+          </GridItemWrapper>
+        ))}
       </GridLayout>
     </Root>
   );
 };
 
-export default GridLayout;
+export default GridComponent;
--- src/main.js
+++ src/main.js
@@ -1,36 +1,44 @@
 import "./main.css";
-import Gridlayout from "./gridlayout";
+import Grid from "./gridlayout"; //gridlayout 컴포넌트 불러오기
 import logo2_img from "./assets/logo2.PNG";
 
 const MainPage = () => {
   return (
-    <div class="mainbox">
+    <div className="mainbox ">
+      {/* 헤더 메뉴 */}
       <header className="header-set align-center">
-        <img class="img_logo2" src={logo2_img} alt="img"></img>
+        <img className="img_logo2" src={logo2_img} alt="img"></img>
         <div className="align-center Tag-list">
-          <div class="Tagbox">
+          <div className="Tagbox">
             <div className="align-center Tag">홈</div>
           </div>
-          <div class="Tagbox">
+          <div className="Tagbox">
             <div className="align-center Tag">플래너</div>
           </div>
-          <div class="Tagbox">
+          <div className="Tagbox">
             <div className="align-center Tag">경비계산 / 준비물</div>
           </div>
-          <div class="Tagbox">
+          <div className="Tagbox">
             <div className="align-center Tag">관광지</div>
           </div>
-          <div class="Tagbox">
+          <div className="Tagbox">
             <div className="align-center Tag">커뮤니티</div>
           </div>
-          <div class="Tagbox">
+          <div className="Tagbox">
             <div className="align-center Tag">오류문의</div>
           </div>
         </div>
       </header>
-      <div class="girdbox">
-        <div class="grid">
-          <Gridlayout></Gridlayout>
+      {/* body */}
+      <div className="content_body align-center">
+        {/* 이미지 영역 */}
+        <div className="img_body align-center">image</div>
+        {/* 그리드 박스 : 플래너 목록 */}
+        <div className="grid_body">
+          <div className="grid_txt">My Trip</div>
+          <div className="grid">
+            <Grid />
+          </div>
         </div>
       </div>
     </div>
